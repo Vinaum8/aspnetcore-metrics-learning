f7f8ccacc8a708a77f05a59c64aab1e15d684f13
vinicius.sfernandes <vinicius.sfernandes@linx.com.br>
2022-11-29 22:04:04 -0300

    reorganização de pastas e simplificando projeto.

---
+++ run.sh
@@ -0,0 +1,10 @@
+# Create folder to Jenkins Files
+mkdir -p jenkins/jenkins_home
+
+# Add ip addres on etc hosts for acess with SNI
+echo '127.0.0.1 net-test.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 grafana-test.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 prometheus-test.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 adminer-test.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 sonarqube-test.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 jenkins-test.com' | sudo tee -a /etc/hosts
