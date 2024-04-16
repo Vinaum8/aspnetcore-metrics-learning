22cff5a3a586a3c9128ead60d291e475159a8cbb
Vinaum8 <vinaumpt@gmail.com>
2024-04-16 11:54:43 -0300

    First Commit: Study with open telemetry.

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
-# Install Docker
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh get-docker.sh
-# sudo usermod -aG docker $USER
-
-# Instal Docker-compose     
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
+#!/bin/bash
 
 # Create folder to Jenkins Files
 mkdir -p jenkins/jenkins_home
