a68d0bfd38d84a2903e8abda8feedc0802ee7df1
Vinaum8 <vinaumpt@gmail.com>
2024-04-16 17:12:54 -0300

    Update program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -3,6 +3,13 @@ using System;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Custom metrics for the application
+var greeterMeter = new Meter("OtPrGrYa.Example", "1.0.0");
+var countGreetings = greeterMeter.CreateCounter<int>("greetings.count", description: "Counts the number of greetings");
+
+// Custom ActivitySource for the application
+var greeterActivitySource = new ActivitySource("OtPrGrJa.Example");
+
 async Task<String> SendGreeting(ILogger<Program> logger)
 {
     // Create a new Activity scoped to the method
@@ -21,5 +28,4 @@ async Task<String> SendGreeting(ILogger<Program> logger)
 }
 
 app.MapGet("/", SendGreeting);
-
 app.Run();
